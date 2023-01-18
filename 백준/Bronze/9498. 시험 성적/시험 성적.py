ff98ba43090822e848ef2880fb1e7aa046ae16da
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-01-18 14:41:23 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+i_score = int(input())
+if i_score >= 90:
+    print('A')
+elif i_score >= 80:
+    print('B')
+elif i_score >= 70:
+    print('C')
+elif i_score >= 60:
+    print('D')
+else:
+    print('F')    
