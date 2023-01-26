8aefa88c810f8fdd95f2a75410f96c6409fb6dd9
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-01-26 11:05:13 +0900

    [Silver IV] Title: 제로, Time: 3852 ms, Memory: 31240 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,12 @@
+i_k = int(input())
+lst_stack = []
+for i_1 in range(i_k):
+    i_input = int(input())
+    if i_input == 0:
+        try:
+            lst_stack.pop()
+        except IndexError:
+            lst_stack.append(0)     
+    else:
+        lst_stack.append(i_input)
+print(sum(lst_stack))
