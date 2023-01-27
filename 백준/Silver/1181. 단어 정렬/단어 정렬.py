6ab4af8b70ea771c2a5bc4c897967696943b9ea3
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-01-27 15:15:49 +0900

    [Silver V] Title: 단어 정렬, Time: 96 ms, Memory: 36204 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,15 +1,6 @@
-import heapq
 import sys
 
-lst_word = []
 i_n = int(sys.stdin.readline())
-for i_1 in range(i_n):
-    s_n = sys.stdin.readline().rstrip()
-    lst_word.append(s_n)
-lst_word = list(set(lst_word))
-
-heap_word = []
-for s_1 in lst_word:
-    heapq.heappush(heap_word, (len(s_1), s_1))
-for i_1 in range(len(heap_word)):
-    print(heapq.heappop(heap_word)[1])
+set_word = set(sys.stdin.readline().rstrip() for i_1 in range(i_n))
+lst_word = sorted(set_word, key= lambda x: (len(x), x))
+print(*lst_word, sep= '\n')
