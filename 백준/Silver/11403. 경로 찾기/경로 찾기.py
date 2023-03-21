647f9d88016dc77248b0f5cbb7b0e31859592112
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-03-21 22:30:55 +0900

    [Silver I] Title: 경로 찾기, Time: 96 ms, Memory: 34120 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+i_n = int(sys.stdin.readline().rstrip())
+graph = []
+for i_1 in range(i_n):
+    ls_node = []
+    ls_input = list(map(int, sys.stdin.readline().split()))
+    for i_2 in range(i_n):
+        if ls_input[i_2] == 1:
+            ls_node.append(i_2)
+    graph.append(ls_node)
+
+for i_3 in range(i_n):
+    ls_visited = [0] * i_n
+    que = deque()
+    for i_3 in graph[i_3]:
+        que.append(i_3)
+    while que:
+        v = que.popleft()
+        if ls_visited[v] == 0:
+            ls_visited[v] = 1
+            for i_4 in graph[v]:
+                que.append(i_4)
+    print(*ls_visited)
