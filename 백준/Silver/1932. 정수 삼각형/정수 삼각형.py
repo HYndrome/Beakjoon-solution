79be332f66292b05c2407245d9cebd29c6cad273
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-04-07 01:32:16 +0900

    [Silver I] Title: 정수 삼각형, Time: 300 ms, Memory: 40636 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,23 @@
+import sys
+
+i_n = int(sys.stdin.readline())
+ls_tri = []
+for i_1 in range(i_n):
+    ls_tri.append(list(map(int, sys.stdin.readline().split())))
+
+ls_topdown = [[-1] * i_1 for i_1 in range(1, i_n + 1)]
+
+def tri(x, y):
+    if x == 0 and y == 0:
+        return ls_tri[y][x]
+    if ls_topdown[y][x] != -1:
+        return ls_topdown[y][x]
+    if x == 0:
+        ls_topdown[y][x] = tri(x, y - 1) + ls_tri[y][x]
+    elif x == y:
+        ls_topdown[y][x] = tri(x - 1, y - 1) + ls_tri[y][x]
+    else:
+        ls_topdown[y][x] = max(tri(x - 1, y - 1), tri(x, y - 1)) + ls_tri[y][x]
+    return ls_topdown[y][x]
+
+print(max([tri(i_2, i_n - 1) for i_2 in range(i_n)]))
