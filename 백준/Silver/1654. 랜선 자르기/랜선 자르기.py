da7aaa4173ecf5a4cc4d1fb0dd62e769f96b3f4e
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-04-20 22:28:26 +0900

    [Silver II] Title: 랜선 자르기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,45 +1,24 @@
 import sys
-i_k , i_n = map(int, input().split())
-lst_lan = [int(sys.stdin.readline()) for i_1 in range(i_k)]
 
-# 이진탐색
-# def binary_search(array, target, start, end):
-#     if start > end:
-#         return None
-#     mid = (start + end) // 2
-#     # 원하는 값 찾은 경우 인덱스 반환
-#     if array[mid] == target:
-#         return mid
-#     elif array[mid] > target:
-#         return binary_search(array, target, start, mid - 1)
-#     elif array[mid] < target:
-#         return binary_search(array, target, mid + 1, end)
-# 결과 값 저장용 리스
+i_k, i_n = map(int, sys.stdin.readline().split())
+ls_lan = [int(sys.stdin.readline()) for _ in range(i_k)]
+
+def lan_cutter(list , object):
+    total_lan = 0
+    for lan in list:
+        total_lan += lan // object
+    return total_lan
+
 ls_result = []
-
-# 케이블을 자르고 개수 세는 함수
-def cablecutter(list, objective):
-    cnt_cable = 0
-    for i_1 in list:
-        cnt_cable += i_1 // objective
-    return cnt_cable
-
-# 이진탐색 함수
-def search_bi(list, target, start, end):
-    # 종료 조건 
+def binery_search(start, end):
     if start > end:
-        return None
+        return
     mid = (start + end) // 2
-
-    if cablecutter(list, mid) == target:
+    if lan_cutter(ls_lan, mid) < i_n:
+        binery_search(start, mid - 1)
+    else:
+        binery_search(mid + 1, end)
         ls_result.append(mid)
-        search_bi(list, target, mid + 1, end)
-    elif cablecutter(list, mid) < target:
-        search_bi(list, target, start, mid - 1)
-    elif cablecutter(list, mid) > target:
-        ls_result.append(mid)
-        search_bi(list, target, mid + 1, end)
 
-search_bi(lst_lan, i_n, 1, 2**31 - 1)
-
+binery_search(1, 2**31 - 1)
 print(max(ls_result))
