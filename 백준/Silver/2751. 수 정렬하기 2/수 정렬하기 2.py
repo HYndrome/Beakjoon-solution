fcd239afa14963794b00957aac41735df7ba33e9
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-05-14 02:10:51 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1196 ms, Memory: 46880 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,13 +1,11 @@
 import sys
 
+
 i_n = int(sys.stdin.readline())
-
-lst_2m = [0 for i_1 in range(2000001)]
-
-for i_2 in range(i_n):
-    i_n1 = int(sys.stdin.readline())
-    lst_2m[i_n1 + 1000000] = 1
-
-for i_3 in range(2000001):
-    if lst_2m[i_3] == True:
-        print(i_3 - 1000000)
+# 문제에서 입력 받는 수는 1000000보다 절댓값이 작거나 같은 정수
+ls_number = [0] * 2000001
+for i in range(i_n):
+    ls_number[int(sys.stdin.readline()) + 1000000] = 1
+for i in range(2000001):
+    if ls_number[i]:
+        print(i - 1000000)
