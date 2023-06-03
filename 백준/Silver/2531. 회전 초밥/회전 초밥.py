d32b02f717a2fd348620d4aff6b046d0ec394702
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-06-03 15:55:34 +0900

    [Silver I] Title: 회전 초밥, Time: 1172 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,12 +1,26 @@
-import sys
-i_n, i_d, i_k, i_c = map(int, sys.stdin.readline().split())
-ls_sushi = [int(sys.stdin.readline()) for _ in range(i_n)]
-cnt_max = 0
-for start in range(len(ls_sushi)):
-    set_sushi = set([ls_sushi[i] for i in range(start, start - i_k, -1)])
-    cnt_current = len(set_sushi)
-    if i_c not in set_sushi:
-        cnt_current += 1
-    if cnt_current > cnt_max:
-        cnt_max = cnt_current
-print(cnt_max)
+N, d, k, c = map(int, input().split())
+
+sushi = [int(input()) for _ in range(N)]
+
+count = 1
+picked = [0 for _ in range(d+1)]
+picked[c] = 1
+
+for i in range(k):
+    if picked[sushi[i]] == 0:
+        count += 1
+    picked[sushi[i]] += 1
+
+answer = count
+for i in range(N-1):
+    picked[sushi[i]] -= 1
+    if picked[sushi[i]] == 0:
+        count -= 1
+    
+    if picked[sushi[(i+k)%N]] == 0:
+        count += 1
+    picked[sushi[(i+k)%N]] += 1
+    
+    answer = max(answer, count)
+
+print(answer)
