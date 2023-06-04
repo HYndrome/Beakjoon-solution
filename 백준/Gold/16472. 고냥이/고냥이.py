113057f56e83d9900054466c279d60a1b4bcfeb3
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-06-04 12:15:10 +0900

    [Gold IV] Title: 고냥이, Time: 132 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/16472. 고냥이/고냥이.py
@@ -0,0 +1,27 @@
+i_n = int(input())
+s_cat = input()
+dct_s = {}
+cnt_max = 0
+cnt_current = 0
+for i in range(len(s_cat)):
+    # 딕셔너리에는 각 문자의 최신 index 값이 저장됨
+    dct_s[s_cat[i]] = i
+    # 딕셔너리의 키 종류가 인식할 수 있는 알파벳 종류 이하일 경우
+    # print(dct_s)
+    if len(dct_s) > i_n:
+        # 딕셔너리의 value 값이 가장 작은 것은 key값을 삭제해야함
+        s_remove = min(dct_s, key=dct_s.get)
+        cnt_current  = i - dct_s[s_remove]
+        dct_s.pop(s_remove)
+        # print(dct_s)
+        # print(cnt_current)
+    # 딕셔너리의 키 종류가 인식할 수 있는 개수 초과일 경우 
+    else:
+        # cnt를 1 올려줌
+        cnt_current += 1
+        # print(dct_s)
+        # print(cnt_current)
+    # 최댓값 갱신 조건문 
+    if cnt_current > cnt_max:
+        cnt_max = cnt_current
+print(cnt_max)
