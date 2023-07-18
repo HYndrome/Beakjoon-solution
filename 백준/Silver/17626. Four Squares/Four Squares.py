44685e5b38cd8f1bf17ab02632458b08779000c0
HYndrome <118808892+HYndrome@users.noreply.github.com>
2023-07-18 11:07:28 +0900

    [Silver III] Title: Four Squares, Time: 228 ms, Memory: 117464 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,19 +1,31 @@
-# dp
+# bfs 최단거리 풀이 - 어림도 없지 시간 초
+from collections import deque
+
 # 입력
 i_n = int(input())
-# 제곱수 구하기
-current_num = 1
+# 방문처리 
+visited = [0] * (i_n + 1)
+# 제곱 수 구하기
 ls_square = []
+num_index = 1
 while True:
-    current_num_square = current_num ** 2
-    ls_square.append(current_num_square)
-    if current_num_square >= i_n:
+    num_square = num_index ** 2
+    ls_square.append(num_square)
+    if num_square >= i_n:
         break
-    current_num += 1
-# print(ls_square)
-ls_num = [0] * (i_n + 1)
-ls_num[1] = 1
-for num in range(2, i_n + 1):
-    ls_num[num] = min([ls_num[num - square] for square in ls_square if square <= num]) + 1
-# print(ls_num)
-print(ls_num[i_n])
+    num_index += 1
+ls_square = list(reversed(ls_square))
+
+que = deque([0])
+while que:
+    current = que.popleft()
+    if current == i_n:
+        break
+    for square in ls_square:
+        next_point = current + square
+        if next_point <= i_n and visited[next_point] == 0:
+            visited[next_point] = visited[current] + 1
+            que.append(next_point)
+        if next_point == i_n:
+            break
+print(visited[i_n])
