c2b2101929d0ecc5260b260b618956d0eda43e7e
HYndrome <118808892+HYndrome@users.noreply.github.com>
2024-12-05 15:59:49 +0900

    [Silver II] Title: 나무 자르기, Time: 584 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,63 +1,61 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int N, M;
-vector<int> trees;
-
-void Input();
-
-long long CutTree(int height);
-
-int BiSearch();
-
-int main()
-{
-	int ans;
-	Input();
-	ans = BiSearch();
-	cout << ans << "\n";
-	return 0;
-}
+int n, m;
+int trees[1000000];
+int tree_max;
 
 void Input()
 {
-	cin >> N >> M;
-	for (int i = 0; i < N; i++)
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
 	{
-		int tree;
-		cin >> tree;
-		trees.push_back(tree);
-	}
-}
-
-long long CutTree(int height)
-{
-	long long cnt = 0;
-	for (int i = 0; i < N; i++)
-	{
-		int tree_taken = trees[i] - height;
-		if (tree_taken > 0)
+		cin >> trees[i];
+		if (trees[i] > tree_max)
 		{
-			cnt += tree_taken;
+			tree_max = trees[i];
 		}
 	}
-
-	return cnt;
 }
 
-int BiSearch()
+long long CutTrees(int height)
+{
+	long long sum_trees = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int left_over = trees[i] - height;
+		if (left_over > 0)
+		{
+			sum_trees += left_over;
+		}
+	}
+	return sum_trees;
+}
+
+int Bisearch()
 {
 	int start = 0;
-	int end = 2000000000;
+	int end = tree_max;
 	while (start <= end)
 	{
 		int mid = (start + end) / 2;
-		long long cnt_current = CutTree(mid);
-		if (cnt_current < M)
+		long long left_over = CutTrees(mid);
+		if (left_over < m)// 나무가 적게 나오면 높이를 낮추고, 많이 나오면 높이를 높여야 함, 같게 나오면 높이를 높여봐야 함
+		{
 			end = mid - 1;
-		else if (cnt_current >= M)
+		}
+		else if (left_over >= m)
+		{
 			start = mid + 1;
+		}
 	}
 	return end;
+}
+
+int main()
+{
+	Input();
+	int ans = Bisearch();
+	cout << ans << "\n";
+	return 0;
 }
