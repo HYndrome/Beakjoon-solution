78c02751c203425e7b622b9dbb3a190c4ee640b7
HYndrome <118808892+HYndrome@users.noreply.github.com>
2024-12-06 10:20:30 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 108 ms, Memory: 4004 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-int dy[] = { 1, 0 , -1, 0 };
-int dx[] = { 0, 1, 0, -1 };
-int N;
+int n;
 int graph[500][500];
-int dp[500][500]; // 해당 자리에서 가장 많이 움직일 수 칸 저장
-int move_max = 0;
-
-
+int dp[500][500];
+int dy[] = { -1, 1, 0, 0 };
+int dx[] = { 0, 0, -1, 1 };
+ 
 void Input()
 {
-	cin >> N;
-	for (int y = 0; y < N; y++)
+	cin >> n;
+	for (int y = 0; y < n; y++)
 	{
-		for (int x = 0; x < N; x++)
+		for (int x = 0; x < n; x++)
 		{
 			cin >> graph[y][x];
 		}
@@ -25,49 +22,40 @@ void Input()
 
 int Dfs(int y, int x)
 {
-	if (dp[y][x] == 0)
+	if (dp[y][x] != 0)
 	{
-		dp[y][x] = 1;
-		for (int i = 0; i < 4; i++)
+		return dp[y][x];
+	}
+	dp[y][x] = 1;
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || ny >= n || nx < 0 || nx >= n)
 		{
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (ny >= N || ny < 0 || nx >= N || nx < 0)
-			{
-				continue;
-			}
-			if (graph[ny][nx] > graph[y][x])
-			{
-				dp[y][x] = max(dp[y][x], Dfs(ny, nx) + 1);
-			}
+			continue;
 		}
+		if (graph[ny][nx] <= graph[y][x])
+		{
+			continue;
+		}
+		dp[y][x] = max(dp[y][x], Dfs(ny, nx) + 1);
 	}
 	return dp[y][x];
 }
 
-void Print()
-{
-	for (int y = 0; y < N; y++)
-	{
-		for (int x = 0; x < N; x++)
-		{
-			cout << dp[y][x] << " ";
-		}
-		cout << "\n";
-	}
-}
 int main()
 {
+	int move_max = 0;
 	Input();
-	for (int y = 0; y < N; y++)
+	for (int y = 0; y < n; y++)
 	{
-		for (int x = 0; x < N; x++)
+		for (int x = 0; x < n; x++)
 		{
-			move_max = max(move_max, Dfs(y, x));
+			int move_current = Dfs(y, x);
+			move_max = max(move_max, move_current);
 		}
 	}
-	cout << move_max;
-
-	// Print();
+	cout << move_max << "\n";
 	return 0;
 }
